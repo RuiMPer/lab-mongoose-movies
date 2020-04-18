856f9fa684f91f627dbaaa7773d04145e0e552a3
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-04-18 18:05:34 +0100

    iteration2

---
+++ starter-code/bin/seed.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Celebrity = require("../models/celebrity");
+const DB_NAME = "lab-movies-project";
+
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const celebrity = [
+	{
+		name: "Rui Pereira",
+		occupation: "ironhacker",
+		catchPhrase: "this is a catch phrase",
+	},
+	{
+		name: "Zé Cabra",
+		occupation: "singer",
+		catchPhrase: "Do you wanna learn to sing",
+	},
+	{
+		name: "DAB",
+		occupation: "ironhacker",
+		catchPhrase: "this is another catch phrase",
+	},
+];
+
+Celebrity.create(celebrity, (err) => {
+	if (err) {
+		throw err;
+	}
+	console.log(`Created ${celebrity.length} celebrities`);
+	mongoose.connection.close();
+});
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router  = express.Router();
 
+const Celebrity = require("../models/celebrity.js")
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/celebrities", (req,res, next) => {
+  const celebrityId = req.query.celebrity_id
+  Celebrity.find()
+  .then((retrieveAllCelebrities) => {
+    // if(err) {
+    //   throw err
+    // } else {
+      console.log(retrieveAllCelebrities)
+      res.render("../celebrities/index", {
+				celebrities: retrieveAllCelebrities,
+			});
+  }) 
+})
 
 module.exports = router;
